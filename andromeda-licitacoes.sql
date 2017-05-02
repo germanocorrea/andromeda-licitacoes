92afdbf6f7e6e5c7607a39975e2d3d624ef7c24f
Germano Correa <germanobruscato@gmail.com>
2017-05-02 10:52:48 -0300

    Update SQL

---
+++ andromeda-licitacoes.sql
@@ -0,0 +1,45 @@
+CREATE TABLE licitations (
+  id int NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  end_date date NOT NULL,
+  open_date date NOT NULL,
+  state varchar(255) NOT NULL,
+
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE licitation_items (
+  id int NOT NULL AUTO_INCREMENT,
+  licitation_id int NOT NULL,
+  quantity int NOT NULL,
+  description varchar(255) NOT NULL,
+
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE proposals (
+  id INT NOT NULL AUTO_INCREMENT,
+  licitation_id INT NOT NULL,
+
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE proposal_items (
+  id INT NOT NULL AUTO_INCREMENT,
+  unity_value FLOAT NOT NULL,
+  proposal_id INT NOT NULL,
+
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  cpf_cnpj INT NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  address  VARCHAR(255),
+  type VARCHAR(255) NOT NULL,
+
+  PRIMARY KEY (id)
+);
