8d1ab68adaa0f499a63a06f3a4fabd94c58c3f1b
Germano Correa <germanobruscato@gmail.com>
2017-05-03 10:41:33 -0300

    Estrutura de LicitationItems

---
+++ app/Controller/LicitationItemsController.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: CTA-ARQUIMEDES-01
+ * Date: 03/05/2017
+ * Time: 10:36
+ */
+class LicitationItemsController extends AppController
+{
+    public function view($licitation_id, $id)
+    {}
+
+    public function edit($licitation_id, $id)
+    {}
+
+    public function add()
+    {}
+
+    public function all()
+    {}
+}
---
+++ app/Model/LicitationItem.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: CTA-ARQUIMEDES-01
+ * Date: 03/05/2017
+ * Time: 10:36
+ */
+class LicitationItem extends AppModel
+{
+
+}
