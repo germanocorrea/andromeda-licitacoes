7fdd51401369892f7f698c63198441de942ed636
Germano Corrêa <germanobruscato@gmail.com>
2017-05-16 19:29:15 -0300

    Update SQL

--- andromeda-licitacoes.sql
+++ andromeda-licitacoes.sql
@@ -1,50 +1,49 @@
-CREATE TABLE licitations (
-  id int NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  end_date date NOT NULL,
-  open_date date NOT NULL,
-  state varchar(255) NOT NULL,
-
-  PRIMARY KEY (id)
+create table licitation_items
+(
+  id int auto_increment
+    primary key,
+  licitation_id int not null,
+  name varchar(255) not null,
+  quantity int not null,
+  description text not null
 );
 
-CREATE TABLE licitation_items (
-  id int NOT NULL AUTO_INCREMENT,
-  licitation_id int NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  quantity int NOT NULL,
-  description text NOT NULL,
-
-  PRIMARY KEY (id)
+create table licitations
+(
+  id int auto_increment
+    primary key,
+  name varchar(255) not null,
+  end_date date not null,
+  open_date date not null,
+  state varchar(255) not null
 );
 
-CREATE TABLE proposals (
-  id INT NOT NULL AUTO_INCREMENT,
-  licitation_id INT NOT NULL,
-
-#   fornecedor ID
-  user_id INT NOT NULL,
-
-  PRIMARY KEY (id)
+create table proposal_items
+(
+  id int auto_increment
+    primary key,
+  unity_value float not null,
+  licitation_item_id int not null,
+  proposal_id int not null
 );
 
-CREATE TABLE proposal_items (
-  id INT NOT NULL AUTO_INCREMENT,
-  unity_value FLOAT NOT NULL,
-  licitation_item_id INT NOT NULL,
-  proposal_id INT NOT NULL,
-
-  PRIMARY KEY (id)
+create table proposals
+(
+  id int auto_increment
+    primary key,
+  licitation_id int not null,
+  user_id int not null,
+  choosed tinyint(1) default '0' null
 );
 
-CREATE TABLE users (
-  id INT NOT NULL AUTO_INCREMENT,
-  cpf_cnpj VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  address  VARCHAR(255),
-  role VARCHAR(255) NOT NULL,
-
-  PRIMARY KEY (id)
+create table users
+(
+  id int auto_increment
+    primary key,
+  cpf_cnpj varchar(255) not null,
+  email varchar(255) not null,
+  name varchar(255) not null,
+  password varchar(255) not null,
+  address varchar(255) null,
+  role varchar(255) not null
 );
